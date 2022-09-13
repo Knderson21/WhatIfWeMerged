f751308c719bebb6a5ede6a8e4a6357b085ab347
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-09-13 15:28:16 -0400

    young chad be feelin

--- WhatIfWeMerged/ClientApp/src/app/app.module.ts
+++ WhatIfWeMerged/ClientApp/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { YoungchadComponent } from './youngchad/youngchad.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'youngchad', component: YoungchadComponent },
     ])
   ],
   providers: [],
